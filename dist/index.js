d8c62b717c34800587f5a6015563652b6ed9fa04
Trond Hindenes <trond@hindenes.com>
2021-09-21 17:11:01 +0200

    updated a thing

--- dist/index.js
+++ dist/index.js
@@ -6331,13 +6331,25 @@ try {
     // `who-to-greet` input defined in action metadata file
     const semVer = core.getInput('SemVer');
     const ShortSha = core.getInput('ShortSha');
+    const useTagIfExists = core.getBooleanInput('useTagIfExists');
+    const githubRef = core.getInput('githubRef')
+    const tagValue = null;
+
+    if (githubRef.startsWith("refs/tags")) {
+        tagValue = a.replace("refs/tags/", "")
+      }
 
     calculatedSemVer = semVer;
     if (semVer.includes('-')) {
         calculatedSemVer = semVer.concat('.', ShortSha);
     }
     console.log(`Calculated version to be ${calculatedSemVer}`);
-    core.setOutput("semver", calculatedSemVer);
+    if (useTagIfExists === true && tagValue != null) {
+        core.setOutput("semver", tagValue);
+    }
+    else {
+        core.setOutput("semver", calculatedSemVer);
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
